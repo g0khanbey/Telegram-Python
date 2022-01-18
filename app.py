1f98eae15f24f337b0f2eeb91a948e25ff34e2fa
Gkhanbey <97981071+Gkhanbey@users.noreply.github.com>
2022-01-18 21:29:40 +0300

    Add files via upload

---
+++ app.py
@@ -0,0 +1,88 @@
+import telegram.ext
+import time
+import telegram
+import os
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+zaman = time.strftime(" %d:%m:%Y %H:%M:%S")
+with open('token.txt', 'r') as f:
+    TOKEN = f.read()
+
+def content1(update, market):
+
+
+
+
+
+def inp():
+    
+    input = update.message.text
+
+
+def test(update, context):
+
+
+
+    
+
+   
+
+  
+
+
+    
+    
+    
+
+
+
+def start(update, context):
+    update.message.reply_text("Merhaba hoşgeldiniz")
+    
+  
+    tests = open('hosgeldin.png', 'rb')
+    time.sleep(3)
+
+    update.message.reply_photo(tests)
+    
+    
+    
+
+
+
+    
+def content(update, kaynak):
+
+#Gokhan Bey
+
+
+
+def contact(update, context):
+    update.message.reply_text("Yet")
+
+updater = telegram.ext.Updater(TOKEN, use_context=True)
+disp = updater.dispatcher
+
+
+disp.add_handler(telegram.ext.CommandHandler("start", start))
+disp.add_handler(telegram.ext.CommandHandler("test", test))
+disp.add_handler(telegram.ext.CommandHandler("market", content1))
+disp.add_handler(telegram.ext.CommandHandler("kaynak", content))
+
+updater.start_polling()
+updater.idle()
+
+
